c3a8eca3f6581a7b75638f556443eb94a1b42977
Maureus <andriiandrusenko@gmail.com>
2020-10-03 11:35:40 +0200

    Init commit
    First solution

---
+++ Exercise01Andrusenko/Exercise01Andrusenko/Exercise01Andrusenko.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+struct Trojuhelnik {
+	int a;
+	int b;
+	int c;
+};
+
+bool lzeSestrojit(Trojuhelnik t) {
+	if (t.a + t.b > t.c && t.a + t.c > t.b && t.b + t.c > t.a)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool lzeSestojitPtr(Trojuhelnik* t) {
+	if (t->a + t->b > t->c && t->a + t->c > t->b && t->b + t->c > t->a)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+int main()
+{
+	int number;
+	std::cout << "Please enter number of triangles you want to create: ";
+	std::cin >> number;
+
+	Trojuhelnik* ukazatel = new Trojuhelnik[number];
+	Trojuhelnik* ptr;
+
+	for (int i = 0; i < number; i++)
+	{
+		std::cout << "Please enter size a: ";
+		std::cin >> ukazatel[i].a;
+		std::cout << "Please enter size b: ";
+		std::cin >> ukazatel[i].b;
+		std::cout << "Please enter size c: ";
+		std::cin >> ukazatel[i].c;
+		ptr = &ukazatel[i];	
+		
+		if (lzeSestojitPtr(ptr))
+		{			
+			int p = ptr->a + ptr->b + ptr->c;
+			std::cout << "Perimetr of triangle is: " << p << std::endl;
+		}
+		else
+			std::cout << "Triangle with this sizes couldn't exist\n";
+	}	
+	
+	delete[] ukazatel;	
+}
