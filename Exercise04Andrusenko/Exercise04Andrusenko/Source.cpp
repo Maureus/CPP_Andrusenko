601e8cbf2d7032f41216af8360edbab3197f8ebc
Maureus <andriiandrusenko@gmail.com>
2020-10-21 19:58:05 +0200

    upd

--- Exercise04Andrusenko/Exercise04Andrusenko/Source.cpp
+++ Exercise04Andrusenko/Exercise04Andrusenko/Source.cpp
@@ -5,6 +5,28 @@
 
 using namespace std;
 
+void sort(vector<Time*> timeVector)
+{
+	int i, j, minIndx;
+
+	for (i = 0; i < timeVector.size() - 1; i++)
+	{
+		minIndx = i;
+		for (j = i + 1; j < timeVector.size(); j++)
+			if (timeVector.at(j)->CompareTo(timeVector.at(minIndx)) == -1)
+				minIndx = j;
+
+		Time* temp = timeVector.at(minIndx);
+		timeVector.at(minIndx) = timeVector.at(i);
+		timeVector.at(i) = temp;
+	}
+
+	for (int k = 0; k < timeVector.size(); k++)
+	{
+		cout << timeVector.at(k)->ToString() << endl;
+	}
+}
+
 int main(int argc, char* argv)
 {
 	try {
@@ -13,33 +35,15 @@ int main(int argc, char* argv)
 		for (int i = 0; i < n; i++)
 		{
 			timeVector.at(i) = new Time(rand() % 25, rand() % 60, rand() % 60);			
-		}
-
-		int i, j, minIndx;
-		 
-		for (i = 0; i < n - 1; i++)
-		{			 
-			minIndx = i;
-			for (j = i + 1; j < n; j++)
-				if(timeVector.at(j)->CompareTo(timeVector.at(minIndx)) == -1)				
-					minIndx = j;
-			
-			Time* temp = timeVector.at(minIndx);
-			timeVector.at(minIndx) = timeVector.at(i);
-			timeVector.at(i) = temp;
-		}
-
-		for (int k = 0; k < timeVector.size(); k++)
-		{
-			cout << timeVector.at(k)->ToString() << endl;
-		}
-
+		}	
 
 		Time t1 = Time(9, 47, 40);
 		Time t2 = Time(11, 27, 1);				
 		cout << t1.ToString() << endl;
 		cout << t2.ToString() << endl;
 		cout << t1.CompareTo(&t2) << endl;
+
+		sort(timeVector);
 
 		for (int k = 0; k < timeVector.size(); k++)
 		{
@@ -57,3 +61,4 @@ int main(int argc, char* argv)
 
 	return 0;
 }
+
