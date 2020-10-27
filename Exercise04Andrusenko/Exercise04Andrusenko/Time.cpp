04994ae23cbbadae56cfbe06270244037902b110
Maureus <andriiandrusenko@gmail.com>
2020-10-27 18:26:57 +0100

    upd

--- Exercise04Andrusenko/Exercise04Andrusenko/Time.cpp
+++ Exercise04Andrusenko/Exercise04Andrusenko/Time.cpp
@@ -15,7 +15,7 @@ Time::~Time()
 
 Time::Time(int hour, int minute, int second)
 {
-	if (hour <= 24 && minute <= 60 && second <= 60 && hour >= 0 && minute >= 0 && second >= 0)
+	if (hour < 24 && minute < 60 && second < 60 && hour >= 0 && minute >= 0 && second >= 0)
 	{
 		this->hour = hour;
 		this->minute = minute;
