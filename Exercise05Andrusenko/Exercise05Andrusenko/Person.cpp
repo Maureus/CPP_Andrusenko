150a9fd68eb4d33144210f24d283da35846ba5ab
Maureus <andriiandrusenko@gmail.com>
2020-11-03 19:08:50 +0100

    Update Person.cpp

--- Exercise05Andrusenko/Exercise05Andrusenko/Person.cpp
+++ Exercise05Andrusenko/Exercise05Andrusenko/Person.cpp
@@ -1,9 +1,14 @@
 #include "Person.h"
+#include <stdexcept>
 
 using namespace std;
 
 Entity::Person::Person(int id, std::string name, std::string phoneNumber)
 {
+	if (id <= 0)
+	{
+		throw invalid_argument("Negative or 0 id!");
+	}
 	this->id = id;
 	this->name = name;
 	this->phoneNumber = phoneNumber;
