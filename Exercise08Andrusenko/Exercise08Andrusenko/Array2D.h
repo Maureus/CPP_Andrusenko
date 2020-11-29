820ad9463adba51f6916247e2c3e8a39566773dc
Maureus <andriiandrusenko@gmail.com>
2020-11-29 19:52:02 +0100

    Init solution

---
+++ Exercise08Andrusenko/Exercise08Andrusenko/Array2D.h
@@ -0,0 +1,324 @@
+﻿#ifndef ARRAY_2D_H
+#define ARRAY_2D_H
+
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+template<typename T>
+class Array2D
+{
+	static_assert(std::is_floating_point<T>::value || std::is_same<T, int>::value || std::is_same<T, long>::value, "Can only be of numeric type!");
+
+	T** array2D;
+	int rows;
+	int columns;
+
+public:
+	// inicializuje matici hodnotami 0
+	Array2D(int rows, int columns);
+
+	// vytvoří matici jako kopii jiné (vytvoří hlubokou kopii)
+	Array2D(const Array2D<T>& array2D);
+	~Array2D();
+	
+	void PutAt(int row, int column, T value);
+
+	// nastaví všechny buňky podle vybraného jednorozměrného pole(předpokládejte,
+	// že rozměr pole je správný; hodnoty v poli jsou uloženy postupně po řádcích)
+	void PutFrom(T* array);
+
+	// pro přístup k buňkám realizujte metody (přístup na neplatný index vyvolá výjimku):	
+	T& GetAt(int row, int column);
+	const T& GetAt(int row, int column) const;
+
+	// metoda vytvoří novou matici požadovaného typu o stejných
+	// rozměrech jako původní matice a převede všechny hodnoty na cílový typ do nové matice
+	template<typename R>
+	Array2D<R> Cast() const;
+
+	// Operace vždy vytvářejí novou matici a nemění aktuální objekt
+	Array2D<T> Transpose() const;
+	Array2D<T> Multiplication(const Array2D& m) const;
+	Array2D<T> Multiplication(T skalar) const;
+	Array2D<T> Addition(const Array2D& m) const;
+	Array2D<T> Addition(T skalar) const;
+
+	// pro porovnání shody dvou matic vytvořte metodu
+	bool IsEqual(const Array2D& m) const;
+
+	void Print() const;
+
+	int GetRows() const;
+	int GetColumns() const;
+	T** GetArray2D() const;
+};
+
+#endif // !ARRAY_2D_H
+
+template<typename T>
+inline Array2D<T>::Array2D(int rows, int columns)
+{
+	if (rows <= 0 || columns <= 0)
+	{
+		throw std::invalid_argument("Number of rows and columns must be bigger than 0!");
+	}
+
+	this->rows = rows;
+	this->columns = columns;
+	array2D = new T * [rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		array2D[i] = new T[columns];
+		for (int j = 0; j < columns; j++)
+		{
+			array2D[i][j] = 0;
+		}
+	}
+}
+
+template<typename T>
+inline Array2D<T>::Array2D(const Array2D<T>& array2D)
+{
+	rows = array2D.rows;
+	columns = array2D.columns;
+	this->array2D = new T * [rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		this->array2D[i] = new T[columns];
+		for (int j = 0; j < columns; j++)
+		{
+			this->array2D[i][j] = array2D.array2D[i][j];
+		}
+	}
+}
+
+template<typename T>
+inline Array2D<T>::~Array2D()
+{
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] array2D[i];
+	}
+
+	delete[] array2D;	
+}
+
+template<typename T>
+inline void Array2D<T>::PutAt(int row, int column, T value)
+{
+	if (row < 0 || row > rows || column < 0 || column > columns)
+	{
+		throw std::out_of_range("Given position is out of range of array!");
+	}
+
+	array2D[row][column] = value;
+}
+
+template<typename T>
+inline void Array2D<T>::PutFrom(T* array)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			array2D[i][j] = array[(i * columns) + j];
+		}
+	}
+}
+
+template<typename T>
+inline T& Array2D<T>::GetAt(int row, int column)
+{
+	if (row < 0 || row > rows || column < 0 || column > columns)
+	{
+		throw std::out_of_range("Given position is out of range of array!");
+	}
+
+	return array2D[row][column];
+}
+
+template<typename T>
+inline const T& Array2D<T>::GetAt(int row, int column) const
+{
+	if (row < 0 || row > rows || column < 0 || column > columns)
+	{
+		throw std::out_of_range("Given position is out of range of array!");
+	}
+
+	return array2D[row][column];
+}
+
+template<typename T>
+inline Array2D<T> Array2D<T>::Transpose() const
+{
+	Array2D<T> newArray2D{ rows, columns };
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			newArray2D.array2D[i][j] = array2D[j][i];
+		}
+	}
+
+	return newArray2D;
+}
+
+template<typename T>
+inline Array2D<T> Array2D<T>::Multiplication(const Array2D& m) const
+{
+	if (columns != m.GetRows())
+	{
+		throw std::invalid_argument("Number of rows and columns must be bigger than 0!");
+	}
+
+	Array2D<T> newArray2D{ rows, columns };
+	auto tempArray = m.GetArray2D();
+
+	for (int i = 0; i < columns; i++)
+	{
+		for (int j = 0; j < m.GetRows(); j++)
+		{
+			T temp = 0;
+
+			for (int k = 0; k < m.GetColumns(); k++)
+			{
+				temp += array2D[i][k] * tempArray[k][j];
+			}
+
+			newArray2D.array2D[i][j] = temp;
+		}
+	}
+
+	return newArray2D;
+}
+
+template<typename T>
+inline Array2D<T> Array2D<T>::Multiplication(T skalar) const
+{
+	Array2D<T> newArray2D{ rows, columns };
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			newArray2D.array2D[i][j] = array2D[i][j] * skalar;
+		}
+	}
+
+	return newArray2D;
+}
+
+template<typename T>
+inline Array2D<T> Array2D<T>::Addition(const Array2D& m) const
+{
+	if (rows != m.rows || columns != m.columns)
+	{
+		throw std::invalid_argument("Arrays must be of the same size!");
+	}
+
+	Array2D<T> newArray2D{ rows, columns };
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			newArray2D.array2D[i][j] = m.array2D[i][j] + array2D[i][j];
+		}
+	}
+
+	return newArray2D;
+}
+
+template<typename T>
+inline Array2D<T> Array2D<T>::Addition(T skalar) const
+{
+	Array2D<T> newArray2D{ rows, columns };
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			newArray2D.array2D[i][j] = array2D[i][j] + skalar;
+		}
+	}
+
+	return newArray2D;
+}
+
+template<typename T>
+inline bool Array2D<T>::IsEqual(const Array2D& m) const
+{
+	if (rows != m.rows || columns != m.columns)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			if (array2D[i][j] != m.array2D[i][j])
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+
+}
+
+template<typename T>
+inline void Array2D<T>::Print() const
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << std::to_string(array2D[i][j]) << "\t";
+		}
+
+		std::cout << std::endl;
+	}
+
+}
+
+template<typename T>
+inline int Array2D<T>::GetRows() const
+{
+	return rows;
+}
+
+template<typename T>
+inline int Array2D<T>::GetColumns() const
+{
+	return columns;
+}
+
+template<typename T>
+inline T** Array2D<T>::GetArray2D() const
+{
+	return array2D;
+}
+
+template<typename T>
+template<typename R>
+inline Array2D<R> Array2D<T>::Cast() const
+{
+	Array2D<R> newArray2D{ rows, columns };
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			newArray2D.PutAt(i, j, static_cast<R>(this->GetAt(i, j)));
+		}
+	}
+
+	return newArray2D;
+}
---
+++ Exercise08Andrusenko/Exercise08Andrusenko/Source.cpp
@@ -0,0 +1,34 @@
+#include "Array2D.h"
+#include <iostream>
+
+int main(int atgv,  char* argc) 
+{
+	try
+	{
+		//Array2D<char*> test{ 3,3 };
+		Array2D<int> m{ 3,3 };
+		int jednodpole[] = { 0,1,2,3,4,5,6,7,8 };
+		m.PutFrom(jednodpole);
+		Array2D<int> mt = m.Transpose();
+		Array2D<int> mmt = m.Multiplication(mt);
+		Array2D<double> mmt_d = mmt.Cast<double>();
+		Array2D<double> n_d{ 3,3 };
+		double jednodpole_d[] = { 4.5,5,0,2,-0.5,7,1.5,9,6 };
+		n_d.PutFrom(jednodpole_d);
+		Array2D<double> mmtn_d = mmt_d.Multiplication(n_d);
+		Array2D<int> r = mmtn_d.Cast<int>();
+		Array2D<int> t{ 3,3 };
+		int tpole[] = { 85,225,236,292,819,866,499,1413,1496 };
+		t.PutFrom(tpole);
+		r.Print();
+		t.Print();
+		std::cout << "r==t ? " << (r.IsEqual(t) ? "true" : "false") << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+
+	return 0;
+}
